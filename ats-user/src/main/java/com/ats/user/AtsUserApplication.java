22d05351e2acf93a91ba8b67fa6df90183c29637
tuttodev27 <tuttopro27@gmail.com>
2026-02-14 00:29:40 -0300

    git commit -m "feat: add domain models and docker-compose/yaml configuration"

---
+++ ats-user/src/main/java/com/ats/user/AtsUserApplication.java
@@ -0,0 +1,13 @@
+package com.ats.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsUserApplication.class, args);
+	}
+
+}
---
+++ ats-user/src/test/java/com/ats/user/AtsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
